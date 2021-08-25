e800a66e261bb5d56d4b2a9e8fdf661d3b87433d
mec-nyan <marianoraspberrypi@gmail.com>
2021-08-25 00:13:57 -0300

    random commit

--- jaskinoid.js
+++ jaskinoid.js
@@ -1,3 +1,9 @@
 // This is one of the ideas for my individual project:
 // A 'breakout' like game written in JS
 console.log('jaskinoid is fun!');
+
+// Just some random stuff by now
+let arr = [1, 2, 3, 4];
+for (let i = 0; i < arr.length; ++i) {
+  console.log(i);
+}
